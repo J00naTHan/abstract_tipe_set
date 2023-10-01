09e3478a0c830341713cedb3e716ae5d4c3f3c6e
J00naTHan <120300587+J00naTHan@users.noreply.github.com>
2023-10-01 20:27:08 -0300

    Create hashTable.py

---
+++ hashTable.py
@@ -0,0 +1,25 @@
+class hashTable:
+  def __init__ (self, size):
+    self.size = size
+    self.table = [None for i in range(self.size)]
+
+  def getHash(self, key):
+    hash = 0
+    key = str(key)
+    for char in key:
+      hash += ord(char)
+    return hash % self.size
+    
+  def __getitem__(self, key):
+    h = self.getHash(key)
+    return self.table[h]
+    
+  def __setitem__(self, key, val):
+    h = self.getHash(key)
+    self.table[h] = val 
+    return self.table[h]
+        
+  def __delitem__(self, key):
+    h = self.getHash(key)
+    self.table[h] = None
+    
