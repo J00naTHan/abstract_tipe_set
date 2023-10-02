6721d7051571aa7ac9708f84c36bb892f3ce43f5
J00naTHan <120300587+J00naTHan@users.noreply.github.com>
2023-10-02 07:00:09 -0300

    Update set.py

--- set.py
+++ set.py
@@ -2,16 +2,16 @@ from hashTable import hashTable
 
 
 class Set(hashTable):
-  def __init__ (self, size, patFactor = 0):
+  def __init__ (self, size):
     super().__init__(size)
-    self.patFactor = self.pattern = patFactor
+    self.pattern = 0
 
   def insert(self, val):
     if self.__getitem__(self.pattern) is None:
       s = self.search(self.pattern)
       if s != val:
         inserted = self.__setitem__(self.pattern, val)
-        self.pattern += 2
+        self.pattern += 1
         return inserted
       else:
         print("O valor inserido já existe!")
@@ -25,7 +25,7 @@ class Set(hashTable):
       delItem = self.__delitem__(key)
       return delItem
     else:
-      return 0
+      return False
   
   def search(self, key):
     if self.__getitem__(key) is not False:
@@ -33,15 +33,45 @@ class Set(hashTable):
     else:
       return False
 
-  def union(self, pset):
-    for key in range(pset.patFactor, pset.size + 1, 2):
+  def union(self, pset, newSize = 10):
+    union = Set(newSize)
+    for key in range(self.size):
+      val = self.search(key)
+      if val is not False and val is not None:
+        union.insert(val)
+    for key in range(pset.size):
+      val = pset.search(key)
+      if val is not False and val is not None and val not in union.table:
+        union.insert(val)
+    return union
+
+  def intersection(self, pset, newSize = 10):
+    intersection = Set(newSize)
+    aux = hashTable(self.size)
+    for key in range(self.size):
+      val = self.search(key)
+      if val is not False and val is not None:
+        aux.__setitem__(key, val)
+    for key in range(pset.size):
       val = pset.search(key)
       if val is not False and val is not None:
-        self.insert(val)
-        pset.delete(key)
+        for i in range(aux.size):
+          otherVal = aux.__getitem__(i)
+          if val == otherVal:
+            intersection.insert(val)
+    return intersection
 
-  def intersection(self, pset):
-    pass
-
-  def difference(self, pset):
-    pass
+  def difference(self, pset, newSize = 10):
+    difference = Set(newSize)
+    aux = hashTable(self.size)
+    for key in range(self.size):
+      val = self.search(key)
+      if val is not False and val is not None:
+        aux.__setitem__(val, key)
+        difference.insert(val)
+    for key in range(pset.size):
+      val = pset.search(key)
+      if val is not False and val is not None and val in difference.table:
+        i = aux.__getitem__(val)
+        difference.delete(i)
+    return difference
